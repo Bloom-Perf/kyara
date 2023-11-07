a4920b406e6f13e5a23bdaea9e73bfba0acfd435
Alexandre Careil (PACL03231) <ext.alexandre.careil@sncf.fr>
2023-11-07 15:55:35 +0100

    scaffold http server

---
+++ src/main/ts/browser.ts
@@ -0,0 +1,102 @@
+import puppeteer, { ConsoleMessage, Page } from "puppeteer";
+import { MetricsEmitter, Status } from "./metrics";
+import chalk from "chalk";
+import { Conf } from "./conf";
+import { Logger } from "winston";
+import { execSync } from "child_process";
+import * as fs from "fs";
+import * as yp from "@bloom-perf/yaml-pptr";
+
+export const launchBrowsers = async (conf: Conf, logger: Logger, me: MetricsEmitter): Promise<void> => {
+
+    try {
+        const runScenarios = yp.readYamlAndInterpret(fs.readFileSync(conf.yamlFilePath, { encoding: "utf8" }));
+
+        // Run the node orchestrator
+        logger.info(`Start browser`);
+
+        // Spawn the browser instance
+        const browser = await puppeteer.launch({
+            headless: conf.headless,
+            ignoreHTTPSErrors: true,
+            product: "firefox",
+            ignoreDefaultArgs: ["--disable-extensions"],
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-accelerated-2d-canvas',
+                '--no-first-run',
+                '--no-zygote',
+                '--single-process',
+                '--disable-gpu',
+                '--ignore-certificate-errors'
+            ]
+        });
+
+        me.browserStarted("firefox", Status.Success);
+
+        // On application exit, close all browsers
+        process.on('exit', async () => {
+            logger.info(`Closing browser`);
+            await browser.close();
+        });
+
+
+        const fetchTabsRamCpuConsumption = () => {
+
+            try {
+                // run the `ls` command using exec
+                const output = execSync("ps aux |grep firefox-bin | grep childID | awk '{print $3 \"-\" $6;}'");
+
+                // log the output received from the command
+                const list: [number, number][] = output.toString().split("\n").map(line => line.split("-").map(str => +str)) as [number, number][];
+
+                let podCpu = 0;
+                let podRam = 0;
+
+                for (let [cpu, ram] of list) {
+                    podCpu += cpu;
+                    podRam += ram;
+                    cpu && ram && me.resourcesConsumptionPerTab("firefox", ram, cpu)
+                }
+
+                podRam && podCpu && me.resourcesConsumptionPerPod("firefox", podRam, podCpu);
+            } catch (e) {
+                logger.error(e);
+            } finally {
+                setTimeout(fetchTabsRamCpuConsumption, 5000);
+            }
+        }
+
+        fetchTabsRamCpuConsumption();
+
+        // Start all tabs and wait for completion
+
+        await runScenarios(browser, async tab => {
+            try {
+                logger.info(`Start browser page now!`);
+                me.browserTabStarted(Status.Success);
+
+                tab.on("request", request => me.browserRequest(new URL(request.url()).hostname))
+                tab.on("requestfinished", request => me.browserRequestFinished(new URL(request.url()).hostname))
+                tab.on("requestfailed", request => me.browserRequestFailed(new URL(request.url()).hostname))
+                tab.on("response", response => me.browserResponse(new URL(response.url()).hostname))
+                tab.on("error", err => {
+                    me.browserError();
+                    logger.info(err.message, { ...err, tags: ["BROWSER_LOG", "BROWSER_ERROR"]/*, url*/ });
+                })
+                tab.on("console", (msg: ConsoleMessage) => {
+                    me.browserLogLine();
+                    logger.info(msg.text(), { tags: ["BROWSER_LOG"] /*, url*/ });
+                });
+            } catch (err) {
+                me.browserTabStarted(Status.Error);
+                logger.error(`Error on page: ${chalk.redBright(err)}`);
+            }
+        });
+
+    } catch (e) {
+        logger.error(`Could not read yaml configuration file "${conf.yamlFilePath}"`)
+    }
+}
---
+++ src/main/ts/conf.ts
@@ -0,0 +1,46 @@
+import chalk from "chalk";
+import fs from "fs";
+import { Logger } from "winston";
+
+export type Conf = {
+    appName: string;
+    yamlFilePath: string;
+    httpPort: number;
+    httpMetricsRoute: string;
+    livenessProbeRoute: string;
+    headless: boolean;
+}
+
+export type ConfOverrides = Partial<{
+    now: Date
+    httpPort: number;
+    httpMetricsRoute: string;
+    livenessProbeRoute: string;
+    headless: boolean;
+}>;
+
+export const logConf = (conf: Conf, logger: Logger) => {
+    logger.debug("Starting DisplayStationDisplay with the following configuration:");
+
+
+    logger.debug(
+        Object
+            .entries(conf)
+            .filter(v => typeof v[1] !== "function")
+            .map(([key, value]) =>
+                "\t- " + chalk.blueBright(key) + "\t-> " + chalk.yellowBright(value.toString())
+            ).join("\n")
+    );
+}
+
+export const createConf = (overrides: ConfOverrides): Conf => {
+
+    return {
+        appName: process.env.KYARA_APP_NAME || "kyara-puppet",
+        yamlFilePath: process.env.KYARA_YAML_FILE_PATH || "/var/conf/kyara.yaml",
+        headless: overrides.headless !== undefined && overrides.headless || !!process.env.KYARA_HEADLESS,
+        httpPort: overrides.httpPort || parseInt(process.env.KYARA_HTTP_PORT || "0"),
+        livenessProbeRoute: overrides.livenessProbeRoute || process.env.KYARA_HTTP_LIVENESS_PROBE_ROUTE || "/live",
+        httpMetricsRoute: overrides.httpMetricsRoute || process.env.KYARA_HTTP_METRICS_ROUTE || "/metrics",
+    };
+};
